f74d99fb49bac6fb51e336584a02fd8bd5efc608
soodeh Ebrahimi <soodeh Ebrahimi>
2021-05-05 22:26:49 +0430

    changes

---
+++ src/components/products/product/product.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+import productStyles from './styles';
+
+const Product = ({ product }) => {
+    const classes = productStyles();
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image={product.image} title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5">
+                        {product.name}
+                    </Typography>
+                    <Typography gutterBottom variant="h5">
+                        {product.price}
+                    </Typography>
+                </div>
+                <Typography variant="body2" color="textSecondary">{product.description}</Typography>
+            </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label="Add to Cart">
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
+
+}
+
+export default Product
